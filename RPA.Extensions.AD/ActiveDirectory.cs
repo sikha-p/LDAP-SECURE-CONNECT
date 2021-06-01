d73a6249c51adeac69c211897f55d66d7bf0476a
Sikha P <sikha.p@AASPL-BRD.COM>
2021-06-01 10:45:34 +0530

    new func UpdateUserMustChangePassword

--- RPA.Extensions.AD/ActiveDirectory.cs
+++ RPA.Extensions.AD/ActiveDirectory.cs
@@ -8,10 +8,10 @@ namespace RPA.Extensions.AD
         public string SetPasswordByAdmin(string ldapPath, string adminUsername, string adminPassword, string searchUserBy, string searchValue, string newPassword)
         {
             // string path = "domain-AD-CA/DC=domain,DC=com";
-            string path = ldapPath;// "AD.domain.com";
+            //string path = ldapPath;// "AD.domain.com";
             try
             {
-                using (var entry = new DirectoryEntry($"LDAP://{path}:636", adminUsername, adminPassword, AuthenticationTypes.SecureSocketsLayer | AuthenticationTypes.Secure))
+                using (var entry = new DirectoryEntry($"LDAP://{ldapPath}:636", adminUsername, adminPassword, AuthenticationTypes.SecureSocketsLayer | AuthenticationTypes.Secure))
                 {
                     using (var searcher = new DirectorySearcher(entry))
                     {
@@ -43,6 +43,45 @@ namespace RPA.Extensions.AD
                 return ex.GetBaseException().ToString();
             }
         }
-            
+
+        public static string UpdateUserMustChangePassword(string ldapPath, string adminUsername, string adminPassword, string userEmail)
+        {
+            try
+            {
+                using (var entry = new DirectoryEntry($"LDAP://{ldapPath}:636", adminUsername, adminPassword, AuthenticationTypes.SecureSocketsLayer | AuthenticationTypes.Secure))
+                {
+                    using (var searcher = new DirectorySearcher(entry))
+                    {
+                        searcher.Filter = $"(&(objectClass=user)(mail={userEmail}*))";
+                        SearchResult result = searcher.FindOne();
+                        if (result != null)
+                        {
+                            var userEntry = result.GetDirectoryEntry();
+                            if (userEntry != null)
+                            {
+                                userEntry.Properties["pwdLastSet"][0] = 0;
+                                userEntry.CommitChanges();
+                                return "Updated User must change password";
+                           
+                            }
+                            else
+                            {
+                                return $"Cannot find the user with Email as {userEmail}";
+                            }
+                        }
+                        else
+                        {
+                            return $"Cannot find the user with Email as {userEmail}";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.GetBaseException().ToString();
+            }
         }
+
+
+    }
 }
