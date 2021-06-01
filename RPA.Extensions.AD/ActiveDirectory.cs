e563d0e96b6ab1d581d90101398314980cce452b
Sikha P <sikha.p@AASPL-BRD.COM>
2021-06-01 14:53:02 +0530

    updates

--- RPA.Extensions.AD/ActiveDirectory.cs
+++ RPA.Extensions.AD/ActiveDirectory.cs
@@ -59,7 +59,12 @@ namespace RPA.Extensions.AD
                             var userEntry = result.GetDirectoryEntry();
                             if (userEntry != null)
                             {
-                                userEntry.Properties["pwdLastSet"][0] = 1;
+                                //Modifications to Pwd-Last-Set attribute #
+                                //The only values that can be set are:
+                                    //  0 -> To set "User Must Change Password at Next Logon", set the pwdLastSet attribute to zero(0). This is as if the Pwd-Last-Set attribute = True - which is an implementation of Password MUST Change condition.
+                                    // -1 -> setting the Pwd-Last-Set attribute attribute to - 1 which will effectively set the Pwd-Last-Set attribute to the current time and remove the "User Must Change Password at Next Logon" restriction.
+                                    // The Pwd-Last - Set attribute attribute cannot be set to any other values except by the system.
+                                userEntry.Properties["pwdLastSet"][0] = -1;
                                 userEntry.CommitChanges();
                                 return "Updated User must change password";
                            
